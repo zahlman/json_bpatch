ad538027c9a63ef369090b232c8eaa374718d2cb
Karl Knechtel <zahlman@gmail.com>
2015-07-11 08:22:02 -0400

    Basic freespace-list implementation

--- src/json_bpatch/constrain.py
+++ src/json_bpatch/constrain.py
@@ -3,6 +3,72 @@ from itertools import chain
 from .pointer import range_intersect
 
 
+class Freespace:
+    """Represents a set of "free" locations in the file to be patched."""
+    def __init__(self):
+        self._ranges = []
+
+
+    def _ranges_with(self, start, size):
+        merged_start = start
+        merged_stop = start + size
+        merged_written = False
+        for r in self._ranges:
+            if r.stop < merged_start:
+                # Completely before the inserted range.
+                yield r
+            elif r.start > merged_stop:
+                # Completely after the inserted range.
+                # Ensure that the merged chunk is written first.
+                if not merged_written:
+                    yield range(merged_start, merged_stop)
+                merged_written = True
+                yield r
+            else:
+                # Overlaps; do merging.
+                merged_start = min(merged_start, r.start)
+                merged_stop = max(merged_stop, r.stop)
+        # Ensure merged chunk is written if there's nothing after it.
+        if not merged_written:
+            yield range(merged_start, merged_stop)
+
+
+    def including(self, start, size):
+        result = Freespace()
+        result._ranges = list(self._ranges_with(start, size))
+        return result
+
+
+    def add(self, start, size):
+        self._ranges = list(self._ranges_with(start, size))
+
+
+    def _ranges_without(self, start, size):
+        removed_start = start
+        removed_stop = start + size
+        for r in self._ranges:
+            if r.stop <= removed_start or r.start >= removed_stop:
+                # No overlap.
+                yield r
+            else:
+                # Do the appropriate clipping.
+                # May produce 0-2 clips.
+                if r.start < removed_start:
+                    yield range(r.start, removed_start)
+                if r.stop > removed_stop:
+                    yield range(removed_stop, r.stop)
+
+
+    def excluding(self, start, size):
+        result = Freespace()
+        result._ranges = list(self._ranges_without(start, size))
+        return result
+
+
+    def remove(self, start, size):
+        self._ranges = list(self._ranges_without(start, size))
+
+
 def range_exclude(r, low, high):
     start, stop, step = r.start, r.stop, r.step
     if low <= r.start < high:
